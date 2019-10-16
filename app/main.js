afc90c5809044ce9f4afb2256d7931b161a86050
Николай Сотула <sotula@itstep1.dp.ua>
2019-10-16 21:18:16 +0300

    add send messaging

--- app/main.js
+++ app/main.js
@@ -5,8 +5,11 @@ const v_chat = new Vue({
         name : 'noname',
         apiKey : '936171315:AAFDPYTgVh68Uh3MRlZGi880CDFdK1keqzY',
         linkUpd : `https://api.telegram.org/bot936171315:AAFDPYTgVh68Uh3MRlZGi880CDFdK1keqzY/getUpdates`,
+        linkSendMsg : `https://api.telegram.org/bot936171315:AAFDPYTgVh68Uh3MRlZGi880CDFdK1keqzY/sendMessage?`,
         remoteData : [],
-        lastId : 0
+        lastId : 0,
+        chats : new Set(),
+        sendMsg : ''
     },
     methods : {
         getUpdates : function(){
@@ -16,22 +19,42 @@ const v_chat = new Vue({
                 1000);
         },
         checkUpdates : function (obj) {
-            console.log(obj);
+            // console.log(obj);
             if(obj[obj.length - 1].update_id != this.lastId){
                 this.lastId = obj[obj.length - 1].update_id;
                 this.remoteData = obj.reverse().map(msg=> {
+                        this.chats.add(msg.message.chat.id);
                         return {
                             name: msg.message.chat.first_name,
                             time: new Date(msg.message.date * 1000).toLocaleString("ru-UA", {hour12: false}),
-                            text : (msg.message.hasOwnProperty('text'))? msg.message.text: msg.message.sticker.emoji
+                            info : this.getText(msg)
                         }
                     }
                 );
-                console.log(this.remoteData);
+                // console.log(this.remoteData);
             }
+        },
+        getText : function(msg){
+            if(msg.message.hasOwnProperty('text')){
+                return {
+                    text: msg.message.text,
+                    isLink: (msg.message.hasOwnProperty('entities'))? true: false
+                };
+            }
+
+            return {
+                text : msg.message.sticker.emoji,
+                isLink : false
+            };
+        },
+        sendMessage2All : function () {
+            this.chats.forEach(chat => fetch(`${this.linkSendMsg}chat_id=${chat}&text=${this.sendMsg}`));
         }
     },
     created : function(){
         this.getUpdates();
     }
 });
+
+//`${this.linkSendMsg}chat_id=${chat}&text=${this.sendMsg}
+// https://api.telegram.org/bot936171315:AAFDPYTgVh68Uh3MRlZGi880CDFdK1keqzY/sendMessage?chat_id=194287825&text=helloblablalba
